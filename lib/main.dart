c9a29200cd0f9d927ece3b5b378b0e3ad8f0e3eb
MarvinBlanca-LIGPH <76429291+MarvinBlanca-LIGPH@users.noreply.github.com>
2021-05-25 17:01:15 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,95 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal.shade800,
+          title: Text(
+            'MiCard',
+            style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 20.0,
+                letterSpacing: 20,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 60.0,
+              foregroundImage: AssetImage('images/profile.png'),
+            ),
+            Text(
+              'Pok E. Mon',
+              style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 35.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  color: Colors.teal[50],
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              width: 200.0,
+              height: 20.0,
+              child: Divider(
+                color: Colors.teal.shade50,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 30.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+63 9123 45678',
+                  style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 30.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'pokemon@poke.com',
+                  style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900),
+                ),
+              ),
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
